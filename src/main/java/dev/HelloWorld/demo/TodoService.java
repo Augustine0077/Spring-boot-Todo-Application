01b519adb7e08c56dc333f0d12c7ce21da8d4fa1
Augustine0077 <augustineshaji007@gmai.com>
2026-03-01 00:52:41 +0530

    Added TodoService

---
+++ src/main/java/dev/HelloWorld/demo/TodoService.java
@@ -0,0 +1,19 @@
+package dev.HelloWorld.demo;
+
+import dev.HelloWorld.demo.models.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository  todoRepository;
+
+    public Todo createTodo(Todo todo){
+        return todoRepository.save(todo);
+    }
+    public Todo getTodoById(Long id){
+        return todoRepository.getReferenceById(id);
+    }
+}
+
