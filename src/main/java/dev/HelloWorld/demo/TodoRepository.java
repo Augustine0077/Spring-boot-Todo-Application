9e86a0208478ff8f57faa4e4994262df8e507e1e
Augustine0077 <augustineshaji007@gmai.com>
2026-03-02 12:25:15 +0530

    added some not in the TodoRepository

--- src/main/java/dev/HelloWorld/demo/TodoRepository.java
+++ src/main/java/dev/HelloWorld/demo/TodoRepository.java
@@ -3,7 +3,6 @@ package dev.HelloWorld.demo;
 import dev.HelloWorld.demo.models.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, Long>
-{
-
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // in this we acre doing the repository things
 }
